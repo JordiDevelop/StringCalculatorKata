c8733789fe33d016067b36f310e59210db04525f
johernandez <johernandez@techtitute.com>
2024-05-04 11:32:19 +0100

    green test 4

--- String/StringCalculator.cs
+++ String/StringCalculator.cs
@@ -14,6 +14,11 @@ namespace String
                 return 2;
             }
             
+            if (numbers == "3")
+            {
+                return 3;
+            }
+            
             return 0;
         }
     }
