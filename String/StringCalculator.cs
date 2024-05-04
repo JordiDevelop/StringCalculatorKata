3c7ef4fda3dd624995fc3a977d852aa3084f8a04
johernandez <johernandez@techtitute.com>
2024-05-04 11:51:34 +0100

    refactor

--- String/StringCalculator.cs
+++ String/StringCalculator.cs
@@ -5,29 +5,16 @@ namespace String
         public static int Add(string numbers)
         {
             var requestNumbers2 = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            if (requestNumbers2.Length > 2)
+            if (requestNumbers2.Length < 1) return 0;
+            
+            int total = 0;
+            foreach (var number in requestNumbers2)
             {
-                int total = 0;
-                foreach (var number in requestNumbers2)
-                {
-                    total += Int32.Parse(number);
-                }
+                total += Int32.Parse(number);
+            }
 
-                return total;
-            }
-            
-            var requestNumbers = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            if (requestNumbers.Length == 2)
-            {
-                return Int32.Parse(requestNumbers[0]) + Int32.Parse(requestNumbers[1]);
-            }
-            
-            if (!string.IsNullOrEmpty(numbers))
-            {
-                return int.Parse(numbers);
-            }
-            
-            return 0;
+            return total;
+
         }
     }
 }
